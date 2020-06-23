d09176691b22d57ecbdfc90f9d8e5d1050456af6
Jeffrey Hoffman <jeffreyhoffmanmba@gmail.com>
2020-06-23 14:37:12 -0400

    update commit

--- PyBank/analysis/PyBank_main_scripts.py
+++ PyBank/analysis/PyBank_main_scripts.py
@@ -1,42 +1,39 @@
-# Create a Python script that analyzes the PyBank records to calculate each of the following:
-
+# First import the os module
 import os
+
+# Module for reading CSV files
 import csv
 
-# initialize the directory
-os.chdir(os.path.dirname(os.path.abspath(__file__))
-
-# Set path for file
-udemy_csv = os.path.join("..", "Resources", "budget_data.csv")
-
-# Create Variables
+#Define the Variables
 months = []
-net_total_profit_loss_amount = x
+net_total_profit_loss_amount = []
 
 count_months = 0
 net_total_profit_loss_amount = 0
 previous_month_profit_loss = 0
 current_month_profit_loss = 0
+profit_loss_change = 0
 
-# Open and read csv
-with open(budget_data_csv_path newline="") as csvfile:
+# initialize the directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-        csv_reader=csv.reader(csvfile, delimiter=",")
+# Set path for file
+budget_data_csv_path = os.path.join('..', 'Resources', 'budget_data.csv')
 
-        #Read the header row first
-        csv_header=next(csvfile)
+# CSV reader specifies delimiter and variable that holds contents
+with open(budget_data_csv_path) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
 
-# The total number of months included in the dataset
-total_months_in_dataset = count_months +=1
+    # Read the header row first
+    csv_header = next(csvfile)
 
-# The net total amount of "Profit/Losses" over the entire period
+    #Prints CSV file header in terminal
+    print(f"CSV Header: {csv_header}")
 
+    # Read each row of data after the header
+    for row in csv_reader:
 
-# The average of the changes in "Profit/Losses" over the entire period
+    # The total number of months included in the dataset
+        count_months +=1
 
-# The greatest increase in profits (date and amount) over the entire period
-
-# The greatest decrease in losses (date and amount) over the entire period
-
-# Print the analysis to the terminal and export a text file with the results
-
+    print(f"Total Months: {count_months}")
